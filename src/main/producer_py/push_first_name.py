27c527b16bbade28c140f2acc344cf5de8a02919
aymen150 <aymen95170@yahoo.fr>
2020-10-16 15:13:23 +0200

    first commit

--- src/main/producer_py/push_first_name.py
+++ src/main/producer_py/push_first_name.py
@@ -3,7 +3,7 @@
 import socket
 import sys
 from time import sleep
-filename = "src/main/resources/data/insee/nat2018.csv"
+filename = "../resources/data/insee/nat2018.csv"
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 9999        # Port to listen on (non-privileged ports are > 1023)
